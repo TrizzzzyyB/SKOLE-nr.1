717bc4d379169017ca74cb709e9ca93bb78d7555
trist <trist@DESKTOP-5KSGM3Q>
2022-09-23 22:43:12 +0200

    Add project files.

---
+++ SKOLE nr.1/Source.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+// opgave 1
+//int main()
+  //{
+	//cout << "hello world";
+  //}
+
+
+  // oppgave 2
+//int main()
+//{
+	//string noe;
+	//cout << "Skriv noe";
+	//cin >> noe;
+	//cout << "her er hva du skrev: " <<noe;
+//}
+
+	// oppgave 3 
+//void main()
+//{
+
+	//string y;
+	//cout << "Hva heter du? ";
+	//cin >> y;
+	//cout << " Hallo " << y << "\n";
+//}
+
+
+/* int main()
+{
+	int i = 1;
+	for (i = 1; i < 5;)
+	{
+		hallo();
+		++i;
+
+	}
+	if (i == 5);
+	{
+		cout << " Stop ";
+	}
+
+
+}
+
+
+char Tristan = 'a';
+int Tristan1 = 1;
+long Tristan2 = 1;
+float Tristan3 = 2;
+double Tristan4 = 3;
+int main()
+{
+	cout << "stoerrelsen paa char = " << sizeof(Tristan) << "\n";
+	cout << "stoerrelsen paa int = " << sizeof(Tristan1) << "\n";
+	cout << "stoerrelsen paa long = " << sizeof(Tristan2) << "\n";
+	cout << "stoerrelsen paa float = " << sizeof(Tristan3) << "\n";
+	cout << "stoerrelsen paa double = " << sizeof(Tristan4) << "\n";
+}
+
+
+	
+	
+	
+
+
